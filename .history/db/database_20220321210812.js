6a94087241a03153412107c82a23cae8a98fe569
SeungYn <kwls0407@gmail.com>
2022-03-21 21:11:59 +0900

    Prep for heroku deployment

--- .history/db/database_20220321210812.js
+++ .history/db/database_20220321210812.js
@@ -2,15 +2,8 @@ import mysql from 'mysql2';
 import { config } from '../config.js';
 import SQ from 'sequelize';
 
-const { host, port, user, database, password } = config.db;
+const { host, user, database, password } = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
   host,
-  port,
   dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
 });
--- .history/db/database_20220321210927.js
+++ .history/db/database_20220321210927.js
@@ -5,7 +5,6 @@ import SQ from 'sequelize';
 const { host, port, user, database, password } = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
   host,
-  port,
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
